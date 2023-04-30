cdf6f87d32a5d6c6a8d136064b6b7aa959d2781f
naiaragabriela <naigabi31@gmail.com>
2023-04-30 19:46:02 -0300

    criacao da externalcityservice e estruturacao das outras pastas

--- AndreTurismoApp.ExternalService/ExternalCityService.cs
+++ AndreTurismoApp.ExternalService/ExternalCityService.cs
@@ -1,13 +1,16 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Net.Sockets;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using AndreTurismoApp.Models;
 using AndreTurismoApp.Models.DTO;
 using Newtonsoft.Json;
 
 namespace AndreTurismoApp.ExternalService
 {
-    public class CityService
+    public class ExternalCityService
     {
         static readonly HttpClient cities = new HttpClient();
   
@@ -28,15 +31,14 @@ namespace AndreTurismoApp.ExternalService
             }
         }
 
-
-        public async Task<List<City>> GetCityById(int id)
+        public async Task<City> GetCityById(int id)
         {
             try
             {
-                HttpResponseMessage response = await cities.GetAsync("https://localhost:8082/api/Cities"+ id);
+                HttpResponseMessage response = await cities.GetAsync("https://localhost:8082/api/Cities/"+ id);
                 response.EnsureSuccessStatusCode();
                 string ender = await response.Content.ReadAsStringAsync();
-                var end = JsonConvert.DeserializeObject<List<City>>(ender);
+                var end = JsonConvert.DeserializeObject<City>(ender);
                 return end;
             }
             catch (HttpRequestException e)
@@ -45,11 +47,24 @@ namespace AndreTurismoApp.ExternalService
             }
         }
 
+        public async Task<HttpStatusCode> PostCity(City city)
+        {
+            HttpResponseMessage response = await cities.PostAsJsonAsync("https://localhost:8082/api/Cities", city);
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> PutCity(City city)
+        {
+            HttpResponseMessage response = await cities.PutAsJsonAsync("https://localhost:8082/api/Cities", city);
+            return response.StatusCode;
+        }
 
 
-
-
-
+        public async Task<HttpStatusCode> DeleteCity(int id)
+        {
+            HttpResponseMessage response = await cities.DeleteAsync("https://localhost:8082/api/Cities/"+ id);
+            return response.StatusCode;
+        }
 
     }
 }
