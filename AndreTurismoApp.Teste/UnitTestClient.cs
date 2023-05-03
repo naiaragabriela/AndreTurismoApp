8e85f687f9914bec2eab98edaae5f4589bbf4c43
naiaragabriela <naigabi31@gmail.com>
2023-05-03 14:17:48 -0300

    atualizacao UnitTestClient

--- AndreTurismoApp.Teste/UnitTestClient.cs
+++ AndreTurismoApp.Teste/UnitTestClient.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using AndreTurismoApp.AddressService.Controllers;
 using AndreTurismoApp.AddressService.Data;
+using AndreTurismoApp.AddressService.Models;
+using AndreTurismoApp.ClientService.Controllers;
 using AndreTurismoApp.ClientService.Data;
+using AndreTurismoApp.ClientService.Models;
+using AndreTurismoApp.Controllers;
 using AndreTurismoApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Xunit;
 
 namespace AndreTurismoApp.Teste
 {
@@ -28,11 +35,12 @@ namespace AndreTurismoApp.Teste
             {
                 context.Client.Add(new Client
                 {
-
+                    Id = 1,
                     Name = "Simone",
                     Phone = "992228974",
                     Address = new()
                     {
+                        Id = 1,
                         Street = "Rua Paulo Chioda",
                         PostalCode = "14890238",
                         Number = 235,
@@ -42,18 +50,20 @@ namespace AndreTurismoApp.Teste
                         City = new City()
                         {
                             Id = 3,
-                            Name = "Jabotical"
+                            Name = "Jaboticabal"
                         }
 
                     }
                 });
+
                 context.Client.Add(new Client
                 {
-
+                    Id = 2,
                     Name = "Naiara",
                     Phone = "992226574",
                     Address = new()
                     {
+                        Id = 2,
                         Street = "Rua Alessio Santini",
                         PostalCode = "14804021",
                         Number = 260,
@@ -64,14 +74,87 @@ namespace AndreTurismoApp.Teste
                         {
                             Id = 2,
                             Name = "Araraquara"
-
                         }
                     }
                 });
 
-
+                context.SaveChanges();
             }
+        }
 
+        [Fact]
+        public async void ShouldBe_ReturnSucess_WhenCall_GetClientById()
+        {
+            InitializeDataBase();
+
+            // Use a clean instance of the context to run the test
+            using (var context = new AndreTurismoAppClientServiceContext(options))
+            {
+                var clientId = 2;
+                var clientController = new ClientsController(context);
+                var client = await clientController.GetClient(clientId);
+                Assert.Equal(2, client.Value?.Id);
+            }
+        }
+
+        [Fact]
+        public async void ShouldBe_ReturnSucess_WhenCall_PostClient()
+        {
+            InitializeDataBase();
+
+            ClientPostRequestDTO client = new()
+            {
+                Name = "Ana",
+                Phone = "33845678",
+                AddressId = 2,
+            };
+
+            // Use a clean instance of the context to run the test
+            using (var context = new AndreTurismoAppClientServiceContext(options))
+            {
+                var clientController = new ClientsController(context);
+                var response = await clientController.PostClient(client);
+                var result = response.Result as CreatedAtActionResult;
+                Assert.NotNull(result?.Value);
+            }
+        }
+
+        [Fact]
+        public async void ShouldBe_ReturnSucess_WhenCall_PutClient()
+        {
+            InitializeDataBase();
+
+            ClientPutRequestDTO client = new()
+            {
+                Name = "Gustavo",
+                Phone = "12345678",
+            };
+
+            // Use a clean instance of the context to run the test
+            using (var context = new AndreTurismoAppClientServiceContext(options))
+            {
+                var clientController = new ClientsController(context);
+                var response = await clientController.PutClient(3, client);
+                var result = response as NoContentResult;
+                Assert.NotNull(result);
+            }
+        }
+
+        [Fact]
+
+        public async void ShouldBe_ReturnSucess_WhenCall_DeleteClient()
+        {
+            InitializeDataBase();
+
+            // Use a clean instance of the context to run the test
+            using (var context = new AndreTurismoAppClientServiceContext(options))
+            {
+                var clientController = new ClientsController(context);
+                var response = await clientController.DeleteClient(2);
+                var result = response as NoContentResult;
+                Assert.NotNull(result);
+            }
         }
     }
+
 }
